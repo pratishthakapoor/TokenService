09c18c59a25e3b2eb12f1e5cb70428e1b10e1d69
Pratishtha <kapoorpratishtha1995@gmail.com>
2018-04-04 21:36:59 +0530

    Intial Version committed

--- Support24/Controllers/MessagesController.cs
+++ Support24/Controllers/MessagesController.cs
@@ -1,8 +1,14 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Autofac;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
 using Microsoft.Bot.Connector;
 
 namespace Support24
@@ -40,6 +46,40 @@ namespace Support24
                 // Handle conversation state changes, like members being added and removed
                 // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
                 // Not available in all channels
+                IConversationUpdateActivity update = message;
+                using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, message))
+                {
+                    //var client = scope.Resolve();
+                    ConnectorClient connector = new ConnectorClient(new Uri(message.ServiceUrl));
+                    if (update.MembersAdded.Any())
+                    {
+                        var replyNow = message.CreateReply();
+                        foreach (var newMember in update.MembersAdded)
+                        {
+                            if (newMember.Id != message.Recipient.Id)
+                            {
+                                List<CardImage> cardImages = new List< CardImage>();
+                                string strCurrentURL = this.Url.Request.RequestUri.AbsoluteUri.Replace(@"/api/messages", "");
+                                string imageURL = String.Format(@"{0}/{1}", strCurrentURL, "Images/support_bot_icon");
+                                cardImages.Add(new CardImage(url: imageURL));
+                                string subtitle = "";
+                                subtitle = @"Welcome!";
+                                HeroCard plCard = new HeroCard()
+                                {
+                                    Title = "HHHHH",
+                                    Subtitle = subtitle,
+                                    Images = cardImages,
+                                    Buttons = null
+                                };
+                                Attachment plAttachment = plCard.ToAttachment();
+                                List<Attachment> alist = new List<Attachment>();
+                                alist.Add(plAttachment);
+                                replyNow.Attachments = alist;
+                                connector.Conversations.ReplyToActivityAsync(replyNow);
+                            }
+                        }
+                    }
+                }
             }
             else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
