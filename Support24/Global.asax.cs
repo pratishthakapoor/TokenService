6d5457cb1445ef67542797606a92cd1e99d9bee3
Rohit Sinha <rohit.sinhalala@gmail.com>
2018-05-04 13:03:33 +0530

    commit chnages

--- Support24/Global.asax.cs
+++ Support24/Global.asax.cs
@@ -1,8 +1,12 @@
 ﻿using Autofac;
+using Microsoft.Bot.Builder.Azure;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
 using Microsoft.Bot.Builder.Internals.Fibers;
+using Microsoft.Bot.Connector;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -27,6 +31,19 @@ namespace Support24
                {
                    builder.RegisterModule(new ReflectionSurrogateModule());
                    builder.RegisterModule<GloabalHandleeMessage>();
+
+                   /*var store = new TableBotDataStore(ConfigurationManager.AppSettings["AzureWebJobsStorage"]);
+
+                   builder.Register(c => store)
+                           .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                           .AsSelf()
+                           .SingleInstance();
+
+                   builder.Register(c => new CachingBotDataStore(store, CachingBotDataStoreConsistencyPolicy.ETagBasedConsistency))
+                           .As<IBotDataStore<BotData>>()
+                           .AsSelf()
+                           .InstancePerLifetimeScope();*/
+
                });
         }
     }
